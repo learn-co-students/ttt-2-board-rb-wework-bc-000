4322d8a32876cd49f3ff12bbda1eaedc1e233e75
Angela Wang <ranran.wang@wework.com>
2018-02-09 03:14:23 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,12 @@
 # Define the variable board below.
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board
+  puts "   |   | "
+  puts "---------"     
+  puts "   |   | "
+  puts "---------"  
+  puts "   |   | "  
+end 
+

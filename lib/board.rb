4c7f9f5ef447cdb9b55fe9e46a90a8347774b51c
Christopher Chan <chris.chan@wework.com>
2018-02-13 04:05:49 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,16 @@
 # Define the variable board below.
+
+board = [" ", " ", " "," ", " ", " "," ", " ", " "]
+
+#creating a method
+def display_board
+puts "   |   |   "
+puts "-----------"      
+puts "   |   |   "
+puts "-----------"   
+puts "   |   |   "
+end
+
+display_board
+  #this is running a method
+  
